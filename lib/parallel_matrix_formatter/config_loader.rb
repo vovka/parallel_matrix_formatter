8f3daeffb45d1e364aba2b4db0616467a88c1887
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2025-06-22 21:00:24 +0300

    Update lib/parallel_matrix_formatter/config_loader.rb

--- lib/parallel_matrix_formatter/config_loader.rb
+++ lib/parallel_matrix_formatter/config_loader.rb
@@ -74,7 +74,7 @@ module ParallelMatrixFormatter
     end
 
     def load_config_file(path)
-      Psych.safe_load(File.read(path))
+      Psych.safe_load(File.read(path), aliases: true)
     rescue Psych::SyntaxError => e
       raise ConfigError, "Invalid YAML in config file #{path}: #{e.message}"
     rescue StandardError => e
